97e6799a1602f383a896bfc64d89e0f50b7652ba
imronmaruf <imronfajar0902@gmail.com>
2023-02-13 21:41:47 +0700

    revisi form login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,58 +1,63 @@
-@include('main-template')
+@include('auth.main')
 
 <body>
-  <div class="container-scroller">
-    <div class="container-fluid page-body-wrapper full-page-wrapper">
-      <div class="content-wrapper d-flex align-items-center auth px-0">
-        <div class="row w-100 mx-0">
-          <div class="col-lg-4 mx-auto">
-            <div class="auth-form-light text-left py-5 px-4 px-sm-5">
-              <div class="brand-logo">
-                <img src="template/images/logo.svg" alt="logo">
-              </div>
-              <h4>Register</h4>
-              <h6 class="fw-light">Register to continue.</h6>
-              <form class="pt-3">
-                <div class="form-group">
-                    <input type="email" class="form-control form-control-lg" id="exampleInputEmail1" placeholder="Username">
-                </div>
-                <div class="form-group">
-                    <input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password">
-                </div>
-                <div class="form-group">
-                    <input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password">
-                </div>
-               
-                <div class="mt-3">
-                  <a class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" href="template/index.html">SIGN IN</a>
-                </div>
-                <div class="mt-2">
-                    <a class="btn btn-block btn-success btn-lg font-weight-medium auth-form-btn" href="/login">LOG IN  </a>
-                  </div>
-                <div class="my-2 d-flex justify-content-between align-items-center">
-                  {{-- <div class="form-check">
+    <div class="container-scroller">
+        <div class="container-fluid page-body-wrapper full-page-wrapper">
+            <div class="content-wrapper d-flex align-items-center auth px-0">
+                <div class="row w-100 mx-0">
+                    <div class="col-lg-4 mx-auto">
+                        <div class="auth-form-light text-left py-5 px-4 px-sm-5">
+                            <div class="brand-logo">
+                                <img src="template/images/logo.svg" alt="logo">
+                            </div>
+                            <h4>Register</h4>
+                            <h6 class="fw-light">Register to continue.</h6>
+                            <form class="pt-3">
+                                <div class="form-group">
+                                    <input type="email" class="form-control form-control-lg" id="exampleInputEmail1"
+                                        placeholder="Username">
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" class="form-control form-control-lg"
+                                        id="exampleInputPassword1" placeholder="Password">
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" class="form-control form-control-lg"
+                                        id="exampleInputPassword1" placeholder="Password">
+                                </div>
+
+                                <div class="mt-3">
+                                    <a class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn"
+                                        href="template/index.html">SIGN IN</a>
+                                </div>
+                                <div class="mt-2">
+                                    <a class="btn btn-block btn-success btn-lg font-weight-medium auth-form-btn"
+                                        href="/login">LOG IN </a>
+                                </div>
+                                <div class="my-2 d-flex justify-content-between align-items-center">
+                                    {{-- <div class="form-check">
                     <label class="form-check-label text-muted">
                       <input type="checkbox" class="form-check-input">
                       Keep me signed in
                     </label>
                   </div>
                   <a href="#" class="auth-link text-black">Forgot password?</a> --}}
-                </div>
-                {{-- <div class="mb-2">
+                                </div>
+                                {{-- <div class="mb-2">
                   <button type="button" class="btn btn-block btn-facebook auth-form-btn">
                     <i class="ti-facebook me-2"></i>Connect using facebook
                   </button>
                 </div> --}}
-              </form>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
+            <!-- content-wrapper ends -->
         </div>
-      </div>
-      <!-- content-wrapper ends -->
+        <!-- page-body-wrapper ends -->
     </div>
-    <!-- page-body-wrapper ends -->
-  </div>
- 
+
 </body>
 
 </html>
